6688916b6c5b4fe9bf420cc5fa22f486636fe42c
Miguel Valera <jhoabran@gmail.com>
2022-07-31 20:06:09 -0400

    archivos reto 9

--- js/index.js
+++ js/index.js
@@ -1,20 +1,25 @@
 function busqueda(){
     var text = document.getElementById("text").value;
+    console.log(text);
     var lista = document.getElementById("list").getElementsByTagName("li");   
-    let i=0
-    let cont=0;
+    var i=0
+    var cont=0;
     for(; i<lista.length;i++){
         if(!lista[i].textContent.includes(text))
             lista[i].style.display="none";
         else{
             cont=cont+1;
+            lista[i].style.display="block";
         }
     }
+    
     if (cont==0){
         var aux = document.getElementById("mensaje");
-        aux.innerHTML="No hubo coincidencias";
-        aux.style.display="block";
-
+        aux.innerHTML="No hay alumnos que tengan en su nombre: "+text;
+        aux.style.display="flex";
+    }else{
+        var aux = document.getElementById("mensaje");
+        aux.style.display="none";
     }
     console.log("done");
 }
@@ -22,10 +27,11 @@ function busqueda(){
 var aux = document.getElementById("titulo");
 aux.innerHTML=config.sitio[0]+config.sitio[1]+" "+config.sitio[2]
 
-aux = document.getElementById("logo");
+aux = document.getElementsByClassName("logo")[0];
+
 aux.innerHTML=config.sitio[0]+config.sitio[1]+" "+config.sitio[2];
 
-aux = document.getElementById("saludo");
+aux = document.getElementsByClassName("saludo")[0];
 aux.innerHTML = config.saludo+", Jose Miguel Valera";
 
 aux = document.getElementById("boton");
@@ -41,8 +47,8 @@ aux = document.getElementById("list");
 for(let i=0;i<listado.length;i++){
     var li= document.createElement("li");
     var img = document.createElement('img');
-    img.src = "28218108\/28218108.jpeg"
-    //img.src = listado[0].imagen
+    //img.src = "28218108\/28218108.jpeg"
+    img.src = listado[i].imagen
     li.appendChild(img);
     li.appendChild(document.createTextNode(listado[i].nombre));
     aux.appendChild(li);
@@ -50,3 +56,4 @@ for(let i=0;i<listado.length;i++){
 
 
 document.getElementById("boton").onclick=busqueda;
+document.getElementById("text").addEventListener('input',busqueda);
