597fc134d4de9b1663e97da347136aa91a4b1a9a
Miguel Valera <jhoabran@gmail.com>
2022-08-03 12:38:10 -0400

    archivos reto 12

--- js/index.js
+++ js/index.js
@@ -1,18 +1,21 @@
 function busqueda(){
     var text = document.getElementById("text").value;
-    console.log(text);
-    var lista = document.getElementById("list").getElementsByTagName("li");   
-    var i=0
-    var cont=0;
-    for(; i<lista.length;i++){
-        if(!lista[i].textContent.includes(text))
-            lista[i].style.display="none";
-        else{
-            cont=cont+1;
-            lista[i].style.display="block";
+
+    
+    let listaux = [];
+    document.getElementsByClassName("carousel-inner w-100")[0].textContent="";
+    let i=0
+    let cont=0
+    for(; i<listado.length;i++){
+        if(listado[i].nombre.includes(text)){
+            listaux[cont]=listado[i];
+            cont++;
         }
     }
     
+    
+    crearItems(listaux)
+
     if (cont==0){
         var aux = document.getElementById("mensaje");
         aux.innerHTML="No hay alumnos que tengan en su nombre: "+text;
@@ -22,6 +25,60 @@ function busqueda(){
         aux.style.display="none";
     }
     console.log("done");
+}
+
+function crearItems(lista){
+    
+    if(lista.length>=1){
+    aux = document.getElementsByClassName("carousel-inner w-100")[0];
+    
+    let div= document.createElement("div");
+    div.classList.add('carousel-item')
+    div.classList.add('active')
+    let div1= document.createElement("div");
+    div1.classList.add('contenido')
+    let img = document.createElement('img');
+    img.style.width="100%"
+    //img.src = "28218108\/28218108.jpeg"
+    img.src = lista[0].imagen
+    div.appendChild(div1);
+    div1.appendChild(img);
+    div1.appendChild(document.createTextNode(lista[0].nombre));
+    aux.appendChild(div);
+
+    for(let i=1;i<lista.length;i++){
+
+        let div= document.createElement("div");
+        div.classList.add('carousel-item')
+        let div1= document.createElement("div");
+        div1.classList.add('contenido')
+        let img = document.createElement('img');
+        img.style.width="100%"
+        //img.src = "28218108\/28218108.jpeg"
+        img.src = lista[i].imagen
+        div.appendChild(div1);
+        div1.appendChild(img);
+        div1.appendChild(document.createTextNode(lista[i].nombre));
+        aux.appendChild(div);
+    }
+    $('.carousel .carousel-item').each(function(){
+    
+        var next = $(this).next();
+        if (!next.length) {
+        next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+        
+        for (var i=0;i<2;i++) {
+            next=next.next();
+            if (!next.length) {
+                next = $(this).siblings(':first');
+              }
+            
+            next.children(':first-child').clone().appendTo($(this));
+          }
+    }); 
+    }
 }
 
 var aux = document.getElementById("titulo");
@@ -41,18 +98,14 @@ aux = document.getElementsByTagName("footer");
 aux[0].innerHTML=config.copyRight;
 
 
+crearItems(listado)
 
-aux = document.getElementById("list");
+ 
 
-for(let i=0;i<listado.length;i++){
-    var li= document.createElement("li");
-    var img = document.createElement('img');
-    //img.src = "28218108\/28218108.jpeg"
-    img.src = listado[i].imagen
-    li.appendChild(img);
-    li.appendChild(document.createTextNode(listado[i].nombre));
-    aux.appendChild(li);
-}
+$('#recipeCarousel').carousel({
+  interval :2000
+})
+
 
 
 document.getElementById("boton").onclick=busqueda;
