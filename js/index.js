202d5a51641fc2b3fe6513a31563054b5902ad99
Valentina Contreras <vcontreras@shokworks.io>
2022-08-04 17:30:39 -0400

    archivo reto 12

--- js/index.js
+++ js/index.js
@@ -1,44 +1,18 @@
-function inicio() {
-  document.getElementsByTagName("title")[0].innerHTML =
-    config.sitio[0] + config.sitio[1] + config.sitio[2];
-  document.getElementsByClassName("logo")[0].innerHTML =
-    config.sitio[0] +
-    "<small>" +
-    config.sitio[1] +
-    "</small>" +
-    config.sitio[2];
-  document.getElementsByClassName("saludo")[0].innerHTML =
-    config.saludo + ", Valentina Contreras";
-  document.getElementsByClassName("nombre")[0].placeholder =
-    config.nombre + "...";
-  document.getElementsByClassName("buscar")[0].value = config.buscar;
-  document.getElementsByTagName("footer")[0].innerHTML = config.copyRight;
+// --------------------- Mis datos -------------------//
+document.getElementsByTagName("title")[0].innerHTML =
+  config.sitio[0] + config.sitio[1] + config.sitio[2];
+document.getElementsByClassName("logo")[0].innerHTML =
+  config.sitio[0] + "<small>" + config.sitio[1] + "</small>" + config.sitio[2];
+document.getElementsByClassName("saludo")[0].innerHTML =
+  config.saludo + ", Valentina Contreras";
+document.getElementsByClassName("nombre")[0].placeholder =
+  config.nombre + "...";
+document.getElementsByClassName("buscar")[0].value = config.buscar;
+document.getElementsByTagName("footer")[0].innerHTML = config.copyRight;
 
-  let listStudent = "";
+let listStudent = "";
 
-  for (let i = 0; i < listado.length; i++) {
-    listStudent +=
-      "<li class='student carousel-item'> <img class='d-block col-3 img-fluid' src=" +
-      listado[i].imagen +
-      "> <p>" +
-      listado[i].nombre +
-      "</p> </li>";
-  }
-
-  const inicio =
-    " <div class='container text-center my-3'> <div class='row mx-auto my-auto'><div id='recipeCarousel' class='carousel slide w-100' data-ride='carousel'><div class='carousel-inner w-100' role='listbox'>";
-
-  const arraytest =
-    "<div class='carousel-item active'> <img class='d-block col-3 img-fluid' src='https://cdn.shopify.com/s/files/1/2304/9095/files/NMSDC.png?10873'></div> <div class='carousel-item'> <img class='d-block col-3 img-fluid' src='https://cdn.shopify.com/s/files/1/2304/9095/files/DBE-ACDBE-logo.png?10873'> </div> <div class='carousel-item'> <img class='d-block col-3 img-fluid' src='https://cdn.shopify.com/s/files/1/2304/9095/files/MBE_LOGO.png?10873'></div><div class='carousel-item'><img class='d-block col-3 img-fluid' src='https://cdn.shopify.com/s/files/1/2304/9095/files/2018_WBENC_logo_text_gray.png?10873'></div><div class='carousel-item'><img class='d-block col-3 img-fluid' src='http://novel-mg.com/assets/cert_logo.png'></div><div class='carousel-item'><img class='d-block col-3 img-fluid' src='https://www.kriaanet.com/wp-content/uploads/2019/02/300ppi-feat-logo_feat_logo-EDWOSB.png'>  </div>";
-
-  const fin = "</div></div></div>";
-
-  document.getElementById("listado").innerHTML = `${inicio}${arraytest}${fin}`;
-  document.getElementById(
-    "imgT"
-  ).innerHTML = `<img class='d-block col-3 img-fluid' src='https://cdn.shopify.com/s/files/1/2304/9095/files/NMSDC.png?10873'>`;
-}
-
+// ------------------- Buscador ----------------------//
 function searchStudent() {
   let listStudent = "";
   let input = document.getElementById("nombre").value;
@@ -60,5 +34,45 @@ function searchStudent() {
     listStudent =
       "<p>" + config.mensaje.replace("[query]", nameIntroduce) + "</p>";
   }
-  document.getElementById("listado").innerHTML = listStudent;
+  // document.getElementById("listado").innerHTML = listStudent;
 }
+
+// ---------------- Listado de estudiantes ---------------//
+for (let i = 0; i < listado.length; i++) {
+  if (i == 0) {
+    listStudent +=
+      "<div class='carousel-item active'> <div class='student'><img src=" +
+      listado[i].imagen +
+      "> <p>" +
+      listado[i].nombre +
+      "</p> </div></div>";
+  } else {
+    listStudent +=
+      "<div class='carousel-item'> <div class='student'><img src=" +
+      listado[i].imagen +
+      "> <p>" +
+      listado[i].nombre +
+      "</p> </div></div>";
+  }
+}
+
+//-------------- JQuery ---------------------//
+$(".carousel-inner").append(listStudent);
+
+$(".carousel").carousel({ interval: 5000 });
+
+$(".carousel .carousel-item").each(function () {
+  let next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(":first");
+  }
+  next.children(":first-child").clone().appendTo($(this));
+
+  for (let i = 0; i < 2; i++) {
+    next = next.next();
+    if (!next.length) {
+      next = $(this).siblings(":first");
+    }
+    next.children(":first-child").clone().appendTo($(this));
+  }
+});
