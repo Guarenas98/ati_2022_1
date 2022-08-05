ec199a2cd0d782b0632a37f4c9d447e722759af5
Luz Rico <luzrico15@gmail.com>
2022-08-05 18:24:49 -0400

    archivos reto 10

--- 21071997/js/javaperfil.js
+++ 21071997/js/javaperfil.js
@@ -9,33 +9,33 @@ Array.prototype.forEach.call(elements, function(element) {
 })
 document.getElementById('imagen').src= perfil.imagen
 
-document.getElementById("conf_home").innerHTML = config.home
+document.getElementById("conf_home").innerHTML = config.home;
 
-document.getElementById("perfil_descripcion").innerHTML = perfil.descripcion
+document.getElementById("perfil_descripcion").innerHTML = perfil.descripcion;
 
-document.getElementById("conf_color").innerHTML = config.color
-document.getElementById("perfil_color").innerHTML = perfil.color
+document.getElementById("conf_color").innerHTML = config.color;
+document.getElementById("perfil_color").innerHTML = perfil.color;
 
-document.getElementById("conf_libro").innerHTML = config.libro
-document.getElementById("perfil_libro").innerHTML = perfil.libro
+document.getElementById("conf_libro").innerHTML = config.libro;
+document.getElementById("perfil_libro").innerHTML = perfil.libro;
 
-document.getElementById("conf_musica").innerHTML = config.musica
-document.getElementById("perfil_musica1").innerHTML = perfil.musica[0]
-document.getElementById("perfil_musica2").innerHTML = perfil.musica[1]
-document.getElementById("perfil_musica3").innerHTML = perfil.musica[2]
+document.getElementById("conf_musica").innerHTML = config.musica;
+document.getElementById("perfil_musica1").innerHTML = perfil.musica[0] + ",";
+document.getElementById("perfil_musica2").innerHTML = perfil.musica[1] + ",";
+document.getElementById("perfil_musica3").innerHTML = perfil.musica[2];
 
 document.getElementById("conf_video_juego").innerHTML = config.video_juego
-document.getElementById("perfil_video_juego1").innerHTML = perfil.video_juego[0]
-document.getElementById("perfil_video_juego2").innerHTML = perfil.video_juego[1]
-document.getElementById("perfil_video_juego3").innerHTML = perfil.video_juego[2]
+document.getElementById("perfil_video_juego1").innerHTML = perfil.video_juego[0] + ",";
+document.getElementById("perfil_video_juego2").innerHTML = perfil.video_juego[1] + ",";
+document.getElementById("perfil_video_juego3").innerHTML = perfil.video_juego[2];
 
-document.getElementById("conf_lenguajes").innerHTML = config.lenguajes
-document.getElementById("perfil_lenguajes1").innerHTML = perfil.lenguajes[0]
-document.getElementById("perfil_lenguajes2").innerHTML = perfil.lenguajes[1]
-document.getElementById("perfil_lenguajes3").innerHTML = perfil.lenguajes[2]
-document.getElementById("perfil_lenguajes4").innerHTML = perfil.lenguajes[3]
+document.getElementById("conf_lenguajes").innerHTML = config.lenguajes;
+document.getElementById("perfil_lenguajes1").innerHTML = perfil.lenguajes[0] + ",";
+document.getElementById("perfil_lenguajes2").innerHTML = perfil.lenguajes[1] + ",";
+document.getElementById("perfil_lenguajes3").innerHTML = perfil.lenguajes[2] + ",";
+document.getElementById("perfil_lenguajes4").innerHTML = perfil.lenguajes[3];
 
-document.getElementById("conf_email").innerHTML = config.email
-document.getElementById("perfil_email").innerHTML = perfil.email
+document.getElementById("conf_email").innerHTML = config.email;
+document.getElementById("perfil_email").innerHTML = perfil.email;
 
-document.getElementById("conf_copyRight").innerHTML = config.copyRight
+document.getElementById("conf_copyRight").innerHTML = config.copyRight;
