fad8575f66c52e3c5316be537830a0127a4c52ef
Hadaya Hadaya <hadaya456@gmail.com>
2022-08-19 20:53:24 -0400

    archivos reto 12

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ function inicio(){
 	document.getElementById("buscar").value = config.buscar;
 	document.getElementById("footer").innerHTML = config.copyRight;
 
-	let lista = "<div class='carousel slide carousel-multi-item' data-ride='carousel'>"
+	let lista = "<div id='carouselExampleControls' class='carousel slide carousel-multi-item' data-ride='carousel'>"
 				+ "<div class='carousel-inner' role='listbox'>"
 				+ "<div class='carousel-item active'>"
 				+ "<div class='row' style='margin-top: 20px;'>";
@@ -17,11 +17,11 @@ function inicio(){
 	for(let i=0; i < listado.length; i++){
 		cont++;
 		lista += "<div class='col-12 col-sm-8 col-md-3' style='width: 17rem; height: 17rem; margin-left: 0px'>"
-			   + "<div class='card'' style='width: 15rem; height: 15rem;'>"
+			   + "<div class='card' style='width: 15rem; height: 15rem;'>"
 			   + "<img class='card-img-top' style='width: 15rem; height: 15rem;' src=" 
 			   + listado[i].imagen + ">"
-			   + "<div class='card-body'><a href='#'>" + 
-			   listado[i].nombre + "</a>"
+			   + "<div class='card-body'><a href='#'>"  
+			   + listado[i].nombre + "</a>"
 			   + "</div></div></div>";
 
 		if(cont == 4){
@@ -31,33 +31,65 @@ function inicio(){
 		}
 	}
 
-	lista += "</div></div>";
+	lista += "</div>"
 
+	+"<a class='carousel-control-prev' href='#carouselExampleControls' role='button' data-slide='prev'>"
+   + "<span class='carousel-control-prev-icon' aria-hidden='true'></span>"
+    +"<span class='sr-only'>Previous</span>"
+  +"</a>"
+  +"<a class='carousel-control-next' href='#carouselExampleControls' role='button' data-slide='next'>"
+  +  "<span class='carousel-control-next-icon' aria-hidden='true'></span>"
+  +  "<span class='sr-only'>Next</span>"
+  +"</a>"
+
+
+	+"</div>";
+console.log(lista);
 	document.getElementById("listado").innerHTML = lista;
-	console.log("Hola");
 }
 
+
+
+
 function buscar(){
-	let lista = "";
 	let input = document.getElementById("entrada").value;
 	aux = input;
 	input = input.toLowerCase();
+	let lista = "<div id='multi-item-example' class='carousel slide carousel-multi-item' data-ride='carousel'>"
+				+ "<div class='carousel-inner' role='listbox'>"
+				+ "<div class='carousel-item active'>"
+				+ "<div class='row' style='margin-top: 20px;'>";
+
+	let cont = 0, lista2 = "";
 
 	for(let i=0; i < listado.length; i++){
 		if(listado[i].nombre.toLowerCase().includes(input.toLowerCase())){
-			lista += "<li class='box' class='card'> <img src=" 
-				  + listado[i].imagen 
-				  + ">"
-				  + "<a href='#'>" + listado[i].nombre 
-				  + "</a>"
-		 		  + "</li>";
+			band = true;
+			cont++;
+			lista2 += "<div class='col-12 col-sm-8 col-md-3' style='width: 17rem; height: 17rem; margin-left: 0px'>"
+				   + "<div class='card' style='width: 15rem; height: 15rem;'>"
+				   + "<img class='card-img-top' style='width: 15rem; height: 15rem;' src=" 
+				   + listado[i].imagen + ">"
+				   + "<div class='card-body'><a href='#'>" + 
+				   listado[i].nombre + "</a>"
+				   + "</div></div></div>";
+
+			if(cont == 4){
+				lista2 += "</div></div><div class='carousel-item'>"
+					   + "<div class='row' style='margin-top: 20px;'>";
+				cont = 0;
+			}
 		}
 	}
 
-	if(lista === ""){
+	if(lista2 === ""){
 		lista =  "<p id='mensaje'>" + config.mensaje.replace("[query]", aux + "</p>");
 	}
-	
+	else{
+		lista += lista2;
+		lista += "</div></div>";
+	}
+
 	document.getElementById("listado").innerHTML = lista;
 	
 }
