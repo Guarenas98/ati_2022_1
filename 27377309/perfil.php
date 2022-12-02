0a0da690034695c751e2320a7fd213173e6ed118
Luis Peña <lfernandopg26@gmail.com>
2022-12-02 17:31:46 -0400

    archivos reto 17

--- 27377309/perfil.php
+++ 27377309/perfil.php
@@ -1,3 +1,16 @@
+<?php
+	$lenguajes = ['ES', 'EN', 'PT'];
+	$len = $lenguajes[0];
+	if (isset($_GET['len'])) {
+		$option = strtoupper($_GET['len']);
+		if (in_array($option, $lenguajes)) {
+			$len = $option;
+		}
+	}
+	$perfil = json_decode(file_get_contents('perfil.json'), true);
+	$config = json_decode(file_get_contents(dirname(dirname(__FILE__)) . "\conf\config{$len}.json"), true);
+?>
+
 <!DOCTYPE HTML>
 <html>
 	<head>
@@ -6,21 +19,19 @@
 		<link rel="icon" href="http://www.ciens.ucv.ve/portalasig2/favicon.ico" type="image/x-icon">
 		<link rel="stylesheet" href="../css/bootstrap.min.css"  type="text/css">
 		<script src="config.json"></script>
-		<script src="perfil.json"></script>
-		<script src="index.js"></script>
 		<script src="../js/bootstrap.bundle.min.js"></script>
 		<script src="../js/snap.svg-min.js"></script>
 		<link rel="stylesheet" href="../css/style.css"  type="text/css">
 		<link rel="stylesheet" href="perfil.css"  type="text/css">
-		<title></title>
+		<title><?= $perfil['nombre']?></title>
 	</head>
 	<body>
 	    <header class="container-fluid p-0">
 			<nav>
 				<ul class="row p-0 m-0">
-					<li class="logo col"></li>
-					<li class="saludo col"></li>
-					<li class="busqueda col"><a href="#"></a></li>
+					<li class="logo col"><?= $config['sitio'][0] . "<small>" . $config['sitio'][1] . "</small> " . $config['sitio'][2] ?></li>
+					<li class="saludo col"><?= $config['saludo'] . ", ". $perfil['nombre'] ?></li>
+					<li class="busqueda col"><a href="#"><?= $config['home'] ?></a></li>
 				</ul>	
 			</nav> 
 	    </header>
@@ -30,10 +41,10 @@
 						<div id="grid" class="grid clearfix">
 							<a href="#" data-path-hover="m 180,34.57627 -180,0 L 0,0 180,0 z">
 								<figure>
-									<img src="" class="img-fluid">
+									<img src="<?= $perfil['imagen'] ?>" class="img-fluid">
 									<svg viewBox="0 0 180 320" preserveAspectRatio="none"><path d="M 180,160 0,218 0,0 180,0 z"/></svg>
 									<figcaption>
-										<h2></h2>
+										<h2><?= $perfil['nombre']?></h2>
 									</figcaption>
 								</figure>
 							</a>
@@ -41,37 +52,36 @@
 				</aside>
 				<div class="col">
 					<main class="p-2">
-						<h1></h1>
-						<p id="descripcion"></p>
+						<h1><?= $perfil['nombre']?></h1>
+						<p id="descripcion"><?= $perfil['descripcion']?></p>
 						<table>
 							<tr id="color">
-								<td></td>
-								<td></td>
+								<td><?= $config['color'] . ": "?></td>
+								<td><?= $perfil['color']?></td>
 							</tr>
 							<tr id="libro">
-								<td></td>
-								<td></td>
+								<td><?= $config['libro']  . ": "?></td>
+								<td><?= $perfil['libro']?></td>
 							</tr>
 							<tr id="musica">
-								<td></td>
-								<td></td>
+								<td><?= $config['musica']  . ": "?></td>
+								<td><?= $perfil['musica']?></td>
 							</tr>
 							<tr id="video_juego">
-								<td></td>
-								<td></td>
+								<td><?= $config['video_juego']  . ": "?></td>
+								<td><?= implode(", ", $perfil['video_juego']) ?></td>
 							</tr>
 							<tr id="lenguajes">
-								<td></td>
-								<td></td>
+								<td><?= $config['lenguajes']  . ": "?></td>
+								<td><?= implode(", ", $perfil['lenguajes']) ?></td>
 							</tr>
 						</table>
-						<p id="contacto"><a href="#"></a></p>
+						<p id="contacto"><?= str_replace("[email]", '<a href="mailto:' . $perfil['email'] . '">' . $perfil['email'] . "</a>", $config['email']) ?></p>
 					</main>
 				</div>
 			</div> 
 	    </section>
-	    <footer class="container-fluid p-2">
-	    </footer>
+	    <footer class="container-fluid p-2"><?= $config['copyRight'] ?></footer>
 
 	   	<script>
 			(function() {
