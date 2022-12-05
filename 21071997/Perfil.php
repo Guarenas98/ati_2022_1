720203cd47cde83f9e25fc123832eb55f2b509c6
Luz Rico <luzrico15@gmail.com>
2022-12-04 22:22:57 -0400

    archivos reto 17

--- 21071997/Perfil.php
+++ 21071997/Perfil.php
@@ -1,10 +1,31 @@
+<?php
+$perfil = file_get_contents("conf/perfil.json");
+$array_perfil = json_decode($perfil, true);
+
+if(isset($_GET['len'])){
+  if ($_GET['len'] == 'es'){
+    $config = file_get_contents("conf/configES.json");
+    $array_config = json_decode($config, true);
+  }else if ($_GET['len'] == 'en'){
+    $config = file_get_contents("conf/configEN.json");
+    $array_config = json_decode($config, true);
+  }else{
+    $config = file_get_contents("conf/configPT.json");
+    $array_config = json_decode($config, true);
+  }
+}else{ //Default en es
+  $config = file_get_contents("conf/configES.json");
+  $array_config = json_decode($config, true);
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en" class="demo-1 no-js">
   <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title class="perfil_nombre"></title>
+    <title class="perfil_nombre"><?= $array_perfil['nombre'] ?></title>
     <link
       rel="icon"
       href="http://www.ciens.ucv.ve/portalasig2/favicon.ico"
@@ -20,11 +41,7 @@
       integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
       crossorigin="anonymous"
     />
-    <script
-      src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-      integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-      crossorigin="anonymous"
-    ></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     <script
       src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"
       integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
@@ -35,8 +52,6 @@
       integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
       crossorigin="anonymous"
     ></script>
-    <script type="text/javascript" src="conf/config.json"></script>
-    <script type="text/javascript" src="conf/perfil.json"></script>
     <link rel="stylesheet" href="css/style2.css" type="text/css" />
     <link rel="stylesheet" href="css/perfil2.css" type="text/css" />
   </head>
@@ -47,8 +62,8 @@
         style="background-color: #86b1ef"
       >
         <span class="navbar-text logo">
-          <span id="conf_sitio1"></span><span id="conf_sitio2"></span>
-          <span id="conf_sitio3"></span>
+          <span id="conf_sitio1"><?= $array_config['sitio'][0] ?></span><span id="conf_sitio2"><?= $array_config['sitio'][1] ?></span>
+          <span id="conf_sitio3"><?= $array_config['sitio'][2] ?></span>
         </span>
         <button
           class="navbar-toggler"
@@ -65,13 +80,22 @@
           <ul class="navbar-nav mr-auto mb-1 mb-lg-0">
             <li class="nav-item active">
               <span class="navbar-text saludo">
-                <span id="conf_saludo"></span>
-                <span id="perfil_nombre1"></span>
+                <span id="conf_saludo"><?= $array_config['saludo'] ?></span>
+                <span id="perfil_nombre1"><?= $array_perfil['nombre'] ?></span>
+              </select>
               </span>
             </li>
           </ul>
-          <span class="navbar-text busqueda mb-2 mb-lg-0">
-            <a href="index.html"><span id="conf_home"></span></a>
+          <span class="navbar-text mb-2 mb-lg-0 d-md-block">
+            <a href="index.html"><span id="conf_home"><?= $array_config['home'] ?></span></a>
+          </span>
+          
+          <span class="navbar-text busqueda mb-2 mb-lg-0 pl-lg-4">
+            <select class="form-control" name="len" id="len">
+              <option value="es" <?= (isset($_GET['len']) && $_GET['len']=='es' ? 'selected' : '') ?>>Español</option>
+              <option value="en" <?= (isset($_GET['len']) && $_GET['len']=='en' ? 'selected' : '') ?>>English</option>
+              <option value="pt" <?= (isset($_GET['len']) && $_GET['len']=='pt' ? 'selected' : '') ?>>Português</option>
+            </select>
           </span>
         </div>
       </nav>
@@ -82,12 +106,12 @@
           <section id="grid" class="grid clearfix">
             <a href="#" data-path-hover="m 180,34.57627 -180,0 L 0,0 180,0 z">
               <figure>
-                <img id="imagen" alt="" class="img-fluid" />
+                <img id="imagen" alt="" class="img-fluid" src="<?= $array_perfil['imagen'] ?>" />
                 <svg viewBox="0 0 180 320" preserveAspectRatio="none">
                   <path d="M 180,160 0,218 0,0 180,0 z" />
                 </svg>
                 <figcaption>
-                  <h2 class="perfil_nombre"></h2>
+                  <h2 class="perfil_nombre"><?= $array_perfil['nombre'] ?></h2>
                   <p>
                     Universidad Central de Venezuela<br />
                     Facultad de Ciencias<br />
@@ -104,49 +128,49 @@
           id="caja"
         >
           <div class="pt-3">
-            <h1 class="perfil_nombre"></h1>
+            <h1 class="perfil_nombre"><?= $array_perfil['nombre'] ?></h1>
             <div class="text-justify">
-              <span id="perfil_descripcion" class="font-italic"></span>
+              <span id="perfil_descripcion" class="font-italic"><?= $array_perfil['descripcion'] ?></span>
             </div>
           </div>
           <div class="pt-3">
             <div>
-              <span id="conf_color"></span> <span id="perfil_color"></span>
+              <span id="conf_color"><?= $array_config['color'] ?></span> <span id="perfil_color"><?= $array_perfil['color'] ?></span>
             </div>
             <div>
-              <span id="conf_libro"></span> <span id="perfil_libro"></span>
+              <span id="conf_libro"><?= $array_config['libro'] ?></span> <span id="perfil_libro"><?= $array_perfil['libro'] ?></span>
             </div>
             <div>
-              <span id="conf_musica"></span>
-              <span id="perfil_musica1"></span>
-              <span id="perfil_musica2"></span>
-              <span id="perfil_musica3"></span>
+              <span id="conf_musica"><?= $array_config['musica'] ?></span>
+              <span id="perfil_musica"><?= implode(", ", $array_perfil['musica']) ?></span>
             </div>
             <div>
-              <span id="conf_video_juego"></span>
-              <span id="perfil_video_juego1"></span>
-              <span id="perfil_video_juego2"></span>
-              <span id="perfil_video_juego3"></span>
+              <span id="conf_video_juego"><?= $array_config['video_juego'] ?></span>
+              <span id="perfil_video_juego"><?= implode(", ", $array_perfil['video_juego']) ?></span>
             </div>
             <div class="font-weight-bold">
-              <span id="conf_lenguajes"></span>
-              <span id="perfil_lenguajes1"></span>
-              <span id="perfil_lenguajes2"></span>
-              <span id="perfil_lenguajes3"></span>
-              <span id="perfil_lenguajes4"></span>
+              <span id="conf_lenguajes"><?= $array_config['lenguajes'] ?></span>
+              <span id="perfil_lenguajes"><?= implode(", ", $array_perfil['lenguajes']) ?></span>
             </div>
           </div>
           <div class="py-3">
-            <span id="conf_email"></span>
-            <a href="#"><span id="perfil_email"></span></a>
+            <span id="conf_email"><?= str_replace('[email]', 
+            '<a href="#"><span id="perfil_email">'.$array_perfil['email'].'</span></a>' , $array_config['email']) ?></span>
           </div>
         </div>
       </div>
     </div>
     <footer class="text-center text-black">
-      <div id="conf_copyRight"></div>
+      <div id="conf_copyRight"><?= $array_config['copyRight'] ?></div>
     </footer>
     <script>
+      $("#len").change(function () {
+        var getUrl = window.location;
+        console.log(getUrl.pathname.split('/')[2])
+        var baseUrl = getUrl .protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1] + "/" + getUrl.pathname.split('/')[2] ;
+        location.href = baseUrl +'?len=' +this.value
+      });
+
       (function () {
         function init() {
           var speed = 250,
@@ -172,9 +196,8 @@
             });
         }
 
-        init();
+        init();      
       })();
     </script>
-    <script type="text/javascript" src="js/javaperfil2.js"></script>
   </body>
 </html>
